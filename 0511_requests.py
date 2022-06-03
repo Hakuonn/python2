a942222efb08cff4660b29b10f0f36ac1f7e749b
Hakuonn <kenny07110226@gmail.com>
2022-06-03 20:31:41 +0800

    first commit

--- 0511_requests.py
+++ 0511_requests.py
@@ -4,8 +4,10 @@ import json
 response = requests.get('https://www.costco.com.tw/store-finder/search?q=%E8%87%BA%E7%81%A3&page=0')
 if response.status_code == 200:
     aa = response.content.decode('utf-8')
+
     # json -> str
     tmp = json.loads(aa)
+    
     # str --> json
     f = open('costco.json' , 'wb')
     aa1 = json.dumps(tmp,indent=4, ensure_ascii=False).encode('utf-8')
--- 0531_pandas.py
+++ 0531_pandas.py
@@ -9,9 +9,10 @@ grade_calculus = pd.Series({'c101': 70, 'c102': 80, 'c103': 10, 'c104': 40})
 grade_total = pd.DataFrame(
     {'python2': grade_python, 'php': grade_php, 'calculus': grade_calculus})
 
+print(grade_total)
 print(grade_total.index)
-print(grade_total.values)
 print(grade_total.columns)
+print(grade_total.values)
 print(grade_total.shape)  # 幾欄幾列
 print(grade_total.head(2))  # 看頭兩筆資料
 print(grade_total.tail(2))  # 看最後兩筆資料
@@ -38,31 +39,31 @@ print(grade_total.iloc[[1, 3], [1, 2]])
 
 print(grade_total.iloc[:, [1, 2]])  # ：-> all
 
-print(grade_total.iloc[ : :-1, : : -1])
+print(grade_total.iloc[::-1, :: -1])
 
 print(grade_total)
-grade_total.iloc[[1,3],[1,2]]=NaN
+grade_total.iloc[[1, 3], [1, 2]] = NaN
 print(grade_total)
-tmp = grade_total.drop('calculus' , axis=1) #刪除直行 -> axis = 1
-tmp = grade_total.drop('c103' , axis=0) #刪除橫列 -> axis = 0
+tmp = grade_total.drop('calculus', axis=1)  # 刪除直行 -> axis = 1
+tmp = grade_total.drop('c103', axis=0)  # 刪除橫列 -> axis = 0
 print(tmp)
 
-tmp1 = grade_total.dropna(axis=0) #把橫列NaN的資料清除掉
+tmp1 = grade_total.dropna(axis=0)  # 把橫列NaN的資料清除掉
 print(tmp1)
 
 
-tmp2 = grade_total.fillna(value=59) #填value給NaN欄位
+tmp2 = grade_total.fillna(value=59)  # 填value給NaN欄位
 print(tmp2)
 
-print(grade_total['python2']>=60)  
-print(grade_total[grade_total['python2']>=60])  #直接把python2當作條件判斷式
+print(grade_total['python2'] >= 60)
+print(grade_total[grade_total['python2'] >= 60])  # 直接把python2當作條件判斷式
 
 
 
 
 
-#範例
-f=open('youbike_station.json' , 'r' , encoding='utf-8')
+# 範例
+f = open('youbike_station.json', 'r', encoding='utf-8')
 data = json.load(f)
 f.close()
 tmp = data['retVal']
@@ -70,25 +71,25 @@ df = pd.DataFrame(tmp)
 print(df)
 
 # 減法方式
-df2 = df.drop(['address_cn' , 'address_en' , 'img'], axis=1)
+df2 = df.drop(['address_cn', 'address_en', 'img'], axis=1)
 print(df2)
 print(df2['area_code'].value_counts())
 df3 = df2[df2['area_code'] == '08']
 print(df3.shape)
 
-#加法方式
-df2 = df[['area_code' , 'district_tw' , 'parking_spaces' , 'name_tw']]
+# 加法方式
+df2 = df[['area_code', 'district_tw', 'parking_spaces', 'name_tw']]
 print(df2)
 print(df2.shape)
 
-print(df2.loc[0 , 'area_code']) #selection by label
-print(df2.iloc[0 , 0]) #selection by postion
+print(df2.loc[0, 'area_code'])  # selection by label
+print(df2.iloc[0, 0])  # selection by postion
 
 print(df2['area_code'].value_counts())
 
 tptmp = df2[df2['area_code'] == '00']
 print(tptmp)
-print(tptmp.groupby('district_tw').count()) #groupby -> 值相同把它算再一起
+print(tptmp.groupby('district_tw').count())  # groupby -> 值相同把它算再一起
 
 result = tptmp.groupby('district_tw').count()['name_tw']
 print(result.sort_values())
